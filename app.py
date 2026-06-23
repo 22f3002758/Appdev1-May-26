3869cfdd3948339180518f291f937a58644fc5cf
Tarika <22f3002758@ds.study.iitm.ac.in>
2026-06-23 12:43:41 +0530

    Database and register page created

--- app.py
+++ app.py
@@ -6,10 +6,36 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///mydb.sqlite3"
     db.init_app(app)
     app.app_context().push()
+    
     return app
 
 app=create_app()
 from backend.routes import *
+from backend.create_initial_data import *
+
+
+# if db.session.query(Services).count()==0:
+#     serv1=Services(name="Home Cleaning", baseprice=500)
+#     serv2=Services(name="Home decor", baseprice=500)
+#     db.session.add_all([serv1,serv2])
+#     db.session.commit()
+
+# if db.session.query(ServiceProvider).count()==0:
+#     sp1=ServiceProvider(email="sp1@gmail.com", password='asdf',serviceid=1)
+#     sp2=ServiceProvider(email="sp2@gmail.com", password='asdf',serviceid=1)
+#     sp3=ServiceProvider(email="sp3@gmail.com", password='asdf',serviceid=2)
+#     sp4=ServiceProvider(email="sp4@gmail.com", password='asdf',serviceid=2)
+#     db.session.add_all([sp1,sp2,sp3,sp4])
+#     db.session.commit()    
+
+# serviceobj=db.session.query(Services).filter_by(id=1).first()  
+# # print(serviceobj)  
+# # print(serviceobj.name)
+# # print(serviceobj.baseprice) 
+
+# sp_obj=db.session.query(ServiceProvider).filter_by(id=1).first()
+# print(sp_obj) 
+# print(sp_obj.service.name)  
 
 if __name__=="__main__":
     app.run(debug=True)
--- backend/routes.py
+++ backend/routes.py
@@ -1,5 +1,51 @@
 from flask import current_app as app
+from flask import render_template, redirect,request
+from backend.models import *
+from datetime import datetime
 
 @app.route('/')
 def home():
-    return("hello from home")
+    return render_template("home.html")
+
+@app.route("/register",methods=["GET","POST"])
+def register():
+    if request.args.get("role")=='customer' and request.method=='GET':
+        return render_template('customer/register.html')
+    if request.args.get("role")=='professional' and request.method=='GET':
+        return render_template('professional/register.html')
+    elif request.method=='POST' and request.args.get("role")=='customer':
+        fname=request.form.get("cust_name")
+        femail=request.form.get("cust_email")
+        fpwd=request.form.get("cust_pwd")
+        faddress=request.form.get("cust_address")
+        fmobile=request.form.get("cust_mobile")
+        cust=db.session.query(Customer).filter_by(email=femail).first()
+        if cust:
+            return "Customer already exist!"
+        else:
+            newcust=Customer(name=fname,email=femail,password=fpwd,address=faddress,mobile=fmobile,status='active')
+            db.session.add(newcust)
+            db.session.commit()
+        return redirect("/login")
+    elif request.method=='POST' and request.args.get("role")=='professional':
+        fname=request.form.get("p_name")
+        femail=request.form.get("p_email")
+        fpwd=request.form.get("p_pwd")
+        faddress=request.form.get("p_address")
+        fmobile=request.form.get("p_mobile")
+        fexp=request.form.get("p_exp")
+        resume=request.files.get("resume")
+        prof=db.session.query(Professional).filter_by(email=femail).first()
+        if prof:
+            return "Professional already exist!"
+        else:
+            # resume.save(f"static/{email}.pdf")
+            newprof=Professional(name=fname,email=femail,password=fpwd,address=faddress,mobile=fmobile,experience=fexp,status='pending',resume_url="#")
+            db.session.add(newprof)
+            db.session.commit()
+        return redirect("/login")
+    
+
+@app.route('/')
+def login():
+    return render_template("login.html")      
